fbcbd58a01d916c18e33c76128856c54194ad241
thomas more <463200698@qq.com>
2023-12-25 15:10:20 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const lz4 = require('lz4')
 const path = require('path');
 const BSON = require('bson')
 const msgpack = require('msgpack-lite');
 const Benchmark = require('benchmark');
 const thriftrw = require('thriftrw')
+const dimbin = require('dimbin')
 const protobuf = require('protobufjs');
 const protobufPerson = protobuf.loadSync('Person.proto');
 const Person = protobufPerson.lookupType('Person');
@@ -12,22 +14,34 @@ var thrift = new thriftrw.Thrift({
   allowOptionalArguments: true,
 });
 
-const jsonData = {name: "John", age: 30};
-const suite = new Benchmark.Suite()
+let jsonData = {name: 
+`Jodfjlakdjflkajdlkfja;`, age: 30};
 
-let json_result,bson_result,msgpack_result,proto_result,thrift_result;
-suite.add('JSON', ()=>{ json_result = JSON.stringify(jsonData)})
-suite.add('BSON', ()=>{ bson_result = BSON.serialize(jsonData)})
-suite.add('msgpack', ()=>{ msgpack_result = msgpack.encode(jsonData)})
-suite.add('protobuf', ()=>{ proto_result = Person.encode(jsonData).finish()})
-suite.add('thrift', ()=>{ thrift_result = thrift.Person.toBuffer(jsonData)})
-suite.add('----------')
-suite.add('JSON—de', ()=>{ JSON.parse(json_result)})
-suite.add('BSON-de', ()=>{ BSON.deserialize(bson_result)})
-suite.add('msgpack-de', ()=>{ msgpack.decode(msgpack_result)})
-suite.add('protobuf-de', ()=>{ Person.decode(proto_result)})
-suite.add('thrift-de', ()=>{ thrift.Person.fromBuffer(thrift_result)})
+//jsonData = JSON.parse(fs.readFileSync('test.json','utf-8').toString())
+lz4.encode(JSON.stringify(jsonData))
 
-suite.on('cycle', (event) => console.log(String(event.target)))
-suite.on('complete', function() {console.log('Fastest is ' + this.filter('fastest').map('name'))})
-suite.run()
+let json_result,bson_result,msgpack_result,proto_result,thrift_result,dimbin_result,lz4_result;
+console.log('----------serialize----------')
+new Benchmark.Suite()
+  .add('JSON', ()=>{ json_result = JSON.stringify(jsonData)})
+  .add('BSON', ()=>{ bson_result = BSON.serialize(jsonData)})
+  .add('msgpack', ()=>{ msgpack_result = msgpack.encode(jsonData)})
+  .add('protobuf', ()=>{ proto_result = Person.encode(jsonData).finish()})
+  .add('thrift', ()=>{ thrift_result = thrift.Person.toBuffer(jsonData)})
+  .add('dimbin', ()=>{ dimbin_result = dimbin.serialize([dimbin.stringsSerialize(jsonData.name),new Uint32Array([30])])})
+  .add('lz4', ()=>{ lz4_result = lz4.encode(json_result)})  //测试序列化和lz4压缩效率
+  .on('cycle', (event) => console.log(String(event.target)))
+  .on('complete', function() {console.log('Fastest is ' + this.filter('fastest').map('name'))})
+  .run()
+console.log('----------deserialize----------')
+new Benchmark.Suite()
+  .add('JSON—de', ()=>{ JSON.parse(json_result)})
+  .add('BSON-de', ()=>{ BSON.deserialize(bson_result)})
+  .add('msgpack-de', ()=>{ msgpack.decode(msgpack_result)})
+  .add('protobuf-de', ()=>{ Person.decode(proto_result)})
+  .add('thrift-de', ()=>{ thrift.Person.fromBuffer(thrift_result)})
+  .add('dimbin-de', ()=>{ o=dimbin.parse(dimbin_result);dimbin.stringsParse(o[0])})
+  .add('lz4-de', ()=>{ lz4.decode(lz4_result)})
+  .on('cycle', (event) => console.log(String(event.target)))
+  .on('complete', function() {console.log('Fastest is ' + this.filter('fastest').map('name'))})
+  .run()
